2781fd7d4e166a5a36778ed08668249dffd5c555
isabelly <isabelly.oficialbr@gmail.com>
2025-05-15 10:56:12 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Cantina1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Projeto_Cantina1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            listBox2 = new ListBox();
+            label2 = new Label();
+            checkBox1 = new CheckBox();
+            checkBox2 = new CheckBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(92, 78);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(315, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 86);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nome";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(93, 119);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(310, 259);
+            listBox1.TabIndex = 2;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(490, 127);
+            button1.Name = "button1";
+            button1.Size = new Size(37, 33);
+            button1.TabIndex = 3;
+            button1.Text = "+";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(422, 127);
+            button2.Name = "button2";
+            button2.Size = new Size(45, 33);
+            button2.TabIndex = 4;
+            button2.Text = "-";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(428, 191);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 36);
+            button3.TabIndex = 5;
+            button3.Text = "Adicionar";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(428, 246);
+            button4.Name = "button4";
+            button4.Size = new Size(104, 32);
+            button4.TabIndex = 6;
+            button4.Text = "Remover";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(428, 305);
+            button5.Name = "button5";
+            button5.Size = new Size(104, 30);
+            button5.TabIndex = 7;
+            button5.Text = "Editar";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(734, 119);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(207, 259);
+            listBox2.TabIndex = 8;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(734, 81);
+            label2.Name = "label2";
+            label2.Size = new Size(181, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Selecione a forma de Pagamento";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(1020, 119);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(82, 19);
+            checkBox1.TabIndex = 10;
+            checkBox1.Text = "checkBox1";
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            checkBox2.AutoSize = true;
+            checkBox2.Location = new Point(1020, 154);
+            checkBox2.Name = "checkBox2";
+            checkBox2.Size = new Size(82, 19);
+            checkBox2.TabIndex = 11;
+            checkBox2.Text = "checkBox2";
+            checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1278, 626);
+            Controls.Add(checkBox2);
+            Controls.Add(checkBox1);
+            Controls.Add(label2);
+            Controls.Add(listBox2);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Label label1;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private ListBox listBox2;
+        private Label label2;
+        private CheckBox checkBox1;
+        private CheckBox checkBox2;
+    }
+}
